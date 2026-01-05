bede34bbd1069a663d0c3dfc58f9140efb230901
Orkhan-Najafli <orkhan.najafli@digirella.az>
2026-01-05 20:49:52 +0400

    auth and business completed

--- internal/http/router.go
+++ internal/http/router.go
@@ -1,26 +1,3 @@
-// package http
-
-// import (
-// 	"net/http"
-
-// 	authHandler "github.com/OrkhanNajaf1i/booking-service/internal/http/handlers/auth"
-// 	businessHandler "github.com/OrkhanNajaf1i/booking-service/internal/http/handlers/business"
-// 	"github.com/OrkhanNajaf1i/booking-service/internal/http/routes"
-// )
-
-// type Handlers struct {
-// 	Business *businessHandler.Handler
-// 	Auth     *authHandler.Handler
-// }
-
-//	func NewRouter(h Handlers) *http.ServeMux {
-//		mux := http.NewServeMux()
-//		routes.RegisterBusinessRoutes(mux, h.Business)
-//		routes.RegisterAuthRoutes(mux, h.Auth)
-//		return mux
-//	}
-//
-// File: internal/http/router.go
 package http
 
 import (
